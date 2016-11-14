27d2581c7924f3406883e22ca51f4b1a840aea23
Osoldier <thomas.ibanez33@gmail.com>
2016-11-14 13:12:22 +0100

    added file reader

--- src/ch/hepia/it/opt/tp2/core/Sudoku.java
+++ src/ch/hepia/it/opt/tp2/core/Sudoku.java
@@ -1,39 +1,57 @@
 package ch.hepia.it.opt.tp2.core;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 /**
- * Created by Thomas on 09.11.16.
+ * Created by Thomas on 09.11.16
  */
 public class Sudoku {
 
     public static void main(String[] args) {
-        int[][] init = {
-                {0, 0, 0, 0, 0, 0, 8, 1, 0},
-                {0, 4, 0, 8, 0, 6, 0, 0, 0},
-                {0, 0, 5, 0, 0, 2, 0, 9, 0},
 
-                {8, 7, 0, 4, 0, 1, 9, 0, 0},
-                {0, 0, 0, 0, 7, 0, 0, 0, 0},
-                {0, 0, 4, 5, 0, 9, 0, 8, 3},
-
-                {0, 6, 0, 1, 0, 0, 4, 0, 0},
-                {0, 0, 0, 6, 0, 5, 0, 2, 0},
-                {0, 2, 8, 0, 0, 0, 0, 0, 0},
-        };
-
-        SudokuState initalState = new SudokuState(init);
+        SudokuState initalState = new SudokuState(readFile("sudoku.txt"));
         System.out.println(initalState);
 
-        SudokuState s = new Sudoku().PSCBacktracking(initalState);
+        Sudoku su = new Sudoku();
+        long t1 = System.currentTimeMillis();
+        SudokuState s = su.PSCBacktracking(initalState);
+        System.out.println(System.currentTimeMillis()-t1);
         System.out.println(s);
+    }
+
+    public static int[][] readFile(String f) {
+        int[][] state = new int[9][9];
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(f));
+            StringBuilder sb = new StringBuilder();
+            String line;
+            int j = 0;
+            int i = 0;
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(" ");
+                for (String s : data) {
+                    state[i][j] = Integer.parseInt(s);
+                    i++;
+                }
+                i = 0;
+                j++;
+            }
+
+            br.close();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+        return state;
     }
 
     public SudokuState PSCBacktracking(SudokuState A) {
         if(A.isComplete()) return A;
-        //System.out.println(A);
         SudokuState r;
         SudokuTile X = A.getMostConstraintVar();
 
